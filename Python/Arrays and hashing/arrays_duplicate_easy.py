a96726e77c03f569f15e1af4a251bf60088795c4
sabarish-p95 <136844331+sabarish-p95@users.noreply.github.com>
2023-06-17 00:21:32 +0530

    Started leetcode problem 1
    Started leetcode problem 1

---
+++ Python/Arrays and hashing/arrays_duplicate_easy.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict_new={}
+        for i in nums:
+            if i in dict_new:
+                dict_new[i] += 1
+            else:
+                dict_new[i] = 1
+        for j in dict_new:
+            if dict_new[j] > 1:
+                return True
+            
+        return False
