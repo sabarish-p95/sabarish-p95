443c9933794276919afc913121a4d24d6791848e
sabarish-p95 <136844331+sabarish-p95@users.noreply.github.com>
2023-06-17 19:07:02 +0530

    Create 2sum.py
    Create 2sum.py

---
+++ Python/Arrays and hashing/2sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 2:
+            if nums[0] + nums[1] != target:
+                return []
+            else:
+                return [0,1]  
+        n = len(nums)-1
+        count =0 
+        for i in range(n):
+            count += 1
+            if(target-nums[i] in nums[i+1:]):
+                new_nums = nums[i+1:]
+                return [i,new_nums.index(target-nums[i])+count]
+
+
+        return []
