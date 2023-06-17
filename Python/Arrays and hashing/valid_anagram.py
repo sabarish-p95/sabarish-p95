968a997a3db3a07314b41743cdf9e636aec287b9
sabarish-p95 <136844331+sabarish-p95@users.noreply.github.com>
2023-06-17 17:37:25 +0530

    create valid_anagram.py
    create valid_anagram.py

---
+++ Python/Arrays and hashing/valid_anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_list =[]
+        t_list =[]
+        for i in s:
+            s_list.append(i)
+        for j in t:
+            t_list.append(j)
+        
+        s_list.sort()
+        t_list.sort()
+        if s_list == t_list:
+            return True
+        return False
